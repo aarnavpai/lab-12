6f64c145d970917abad373142fc23581d57ec06e
Aarnav Pai <187662590+aarnavpai@users.noreply.github.com>
2025-04-16 15:37:05 +0530

    fix: cors

--- backend/main.py
+++ backend/main.py
@@ -2,12 +2,22 @@ from fastapi import FastAPI
 from routes.items import router as items_router
 from routes.analytics import router as analytics_router
 from routes.quiz import router as quiz_router
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
+
+# fix cors
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 app.include_router(items_router, prefix="/items")
 app.include_router(analytics_router)
 app.include_router(quiz_router)
+
 
 # why the hell did I write this function?
 @app.get("/home")
