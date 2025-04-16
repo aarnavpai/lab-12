e8ec59f2e4b20856bbbce668bb58229d8547d62e
Aarnav Pai <187662590+aarnavpai@users.noreply.github.com>
2025-04-16 15:53:03 +0530

    fix: post -> get

--- backend/routes/users.py
+++ backend/routes/users.py
@@ -4,11 +4,14 @@ from bson import ObjectId
 
 router = APIRouter()
 
+
 async def get_users_collection():
     from db import init_db
+
     return init_db()["users_collection"]
 
-@router.post("/")
+
+@router.get("/")
 async def get_users():
     collection = await get_users_collection()
     users = []
@@ -17,12 +20,14 @@ async def get_users():
         users.append(user)
     return users
 
+
 # whats ur favorite genre of music ??? mine is EDM
 @router.post("/")
 async def create_user(user: User):
     collection = await get_users_collection()
     result = await collection.insert_one(user.dict())
     return {"id": str(result.inserted_id)}
+
 
 @router.delete("/{user_id}")
 async def delete_user(user_id: str):
